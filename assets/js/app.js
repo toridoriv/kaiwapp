6a3fabb5887823191bd937c4867f6fbb792eabb2
malditoprotozoo <virodriguez.fe@gmail.com>
2017-12-16 01:34:59 -0300

    Login Screen Ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,36 @@
+var loginUsername = "#login-username";
+var pass = "#pass";
+var loginAvatarURL = "#login-avatar-url";
+var loginBtn = "#login-btn";
+var passwd = "kaiwappLogin//88";
+var checkImg = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|png|svg)/;
+
+
 $(document).ready(function() {
+
   setTimeout(function() {
     $("#loader").fadeOut();
-  }, 2000);
+  }, 5000);
+
+  $("#login-username, #pass, #login-avatar-url").keyup(function() {
+    if ($(loginUsername).val().length > 0 && $(pass).val().length > 0 && $(loginAvatarURL).val().length > 0) {
+      $(loginBtn).removeClass("disabled");
+    } else {
+      $(loginBtn).addClass("disabled");
+    }
+  });
+
+  $(loginBtn).click(function() {
+    if ($(pass).val() == passwd && checkImg.test($(loginAvatarURL).val())) {
+      window.location.replace("app.html");
+    } else {
+      alert("Input not valid");
+    }
+  });
 });
+
+/**
+* $("#login-logo").click(function() {
+*  $("#login-logo").effect("pulsate", {times: 2}, 1000);
+* });
+*/
