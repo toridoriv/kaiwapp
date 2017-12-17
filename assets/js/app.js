01498e20f65db0628ffcbedec34d6f3bc1828c3f
malditoprotozoo <virodriguez.fe@gmail.com>
2017-12-16 21:05:48 -0300

    Main container ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,8 @@ var loginAvatarURL = "#login-avatar-url";
 var loginBtn = "#login-btn";
 var passwd = "kaiwappLogin//88";
 var checkImg = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|png|svg)/;
+var textaMsg = $("#textarea-msg");
+var sendBtn = $("#send-btn");
 
 
 $(document).ready(function() {
@@ -37,6 +39,21 @@ $(document).ready(function() {
     $(this).mouseleave(function() {
       $(".hamburger").css({"opacity":"1"});
     });
+  });
+
+  $(textaMsg).keyup(function() {
+    if ($(this).val().length > 0) {
+      $(sendBtn).removeClass("disabled");
+    } else {
+      $(sendBtn).addClass("disabled");
+    }
+  });
+
+  $("#send-btn").click(function() {
+    var msg = $(textaMsg).val().trim();
+    $(textaMsg).val("");
+    $(this).addClass("disabled");
+    console.log(msg);
   });
 });
 
